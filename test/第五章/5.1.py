5a9d05c6204c0065d193bd6ba9b0d7f2224b9f8e
zhanglei12306 <1044570046@qq.com>
2023-02-12 21:46:55 +0800

    集合

---
+++ test/第五章/5.1.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+#打开百度首页
+driver.get('https://www.baidu.com')
+#在搜索输入框中输入文本"python"
+driver.find_element(By.ID, 'kw') .send_keys('python')
+
