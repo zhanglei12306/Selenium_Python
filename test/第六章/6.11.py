eee82053ebd6c64e9d2985b9ef4129297a1ff4b7
zhanglei12306 <1044570046@qq.com>
2023-02-13 21:13:52 +0800

    退出webdriver

---
+++ test/第六章/6.11.py
@@ -0,0 +1,7 @@
+#coding=utf-8
+from selenium import webdriver
+driver = webdriver.Chrome()
+driver.get('https://www.baidu.com/')
+driver.find_element_by_id('kw').send_keys('Selenium')
+# 退出webdriver
+driver.quit()
