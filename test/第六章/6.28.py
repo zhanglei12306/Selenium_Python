fcd5f1f751c4f28513b1372156daf518c76cab81
zhanglei12306 <1044570046@qq.com>
2023-02-13 21:54:17 +0800

    js操作

--- test/第六章/6.28.py
+++ test/第六章/6.28.py
@@ -1,8 +1,10 @@
-	#coding=utf-8
+# coding=utf-8
 from selenium import webdriver
+
 driver = webdriver.Chrome()
 driver.maximize_window()
 driver.get("https://www.baidu.com")
-js = "document.getElementById('kw').value = ¡®selenium'"
+js = "document.getElementById('kw').value = â€˜selenium'"
 driver.execute_script(js)
 driver.quit()
+
