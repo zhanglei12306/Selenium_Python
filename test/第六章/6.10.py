a58cb6342b48e41053cfa5ffc7f81732084ae19f
zhanglei12306 <1044570046@qq.com>
2023-02-13 21:11:51 +0800

    清除输入框

--- test/第六章/6.10.py
+++ test/第六章/6.10.py
@@ -3,4 +3,5 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get('https://www.baidu.com/')
 driver.find_element_by_id('kw').send_keys('Selenium')
+# 清除输入框
 driver.find_element_by_id('kw').clear()
