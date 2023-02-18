626cc36ead03b898d8ede7a63fe35f135295a9c0
zhanglei12306 <1044570046@qq.com>
2023-02-18 17:05:18 +0800

    contains模糊定位

---
+++ test/第七章/7.41.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+from selenium import webdriver
+driver = webdriver.Chrome()
+#以下请输入html文件的完整路径。
+driver.get("xxx/beijing.html")
+res = driver.find_elements_by_xpath("//*[contains(@id,'beijing')]")
+
+for r in res:
+    print(r)
+    print(type(r))
