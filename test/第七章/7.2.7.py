ab6e1993a256a6c904bf32701c3503f62bd4c61a
zhanglei12306 <1044570046@qq.com>
2023-02-18 16:53:24 +0800

    字典清空

---
+++ test/第七章/7.2.7.py
@@ -0,0 +1,10 @@
+dict_1 = {'Name': 'Jack','Age':18,'Score':100}
+print("操作字典元素之前，遍历并打印字典元素如下：")
+for (key,value) in dict_1.items():
+    print(key + ":" + str(value))
+
+dict_1.clear()
+print("操作字典元素之后，遍历并打印字典元素如下：")
+print(dict_1)
+for (key,value) in dict_1.items():
+    print(key + ":" + str(value))
