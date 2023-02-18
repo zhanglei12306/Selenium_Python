5576b9fad4162d00fa9f7973ad9224fb21231a2e
zhanglei12306 <1044570046@qq.com>
2023-02-18 16:50:39 +0800

    遍历字段

--- test/第七章/7.2.3.py
+++ test/第七章/7.2.3.py
@@ -2,3 +2,7 @@
 dic ={"android":"appium","web":"selenium","interface":"python interface automation"}
 for i in dic:
     print(i,dic[i])
+
+dic ={"android":"appium","web":"selenium","interface":"python interface automation"}
+for key, value in dic.items():
+    print(key, value)
