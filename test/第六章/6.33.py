96f567b57767d941173b960e9ccc5b1805b43431
zhanglei12306 <1044570046@qq.com>
2023-02-18 16:18:04 +0800

    驱动切换到iframe

--- test/第六章/6.33.py
+++ test/第六章/6.33.py
@@ -5,6 +5,8 @@ driver = webdriver.Chrome()
 driver.get('https://en.mail.qq.com/cgi-bin/loginpage')
 #驱动切换到iframe
 driver.switch_to.frame("login_frame")
+#驱动切换到iframe外
+# driver.switch_to.default_content("login_frame")
 #对用户名赋值
 driver.find_element_by_id('u').send_keys('test')
 #退出浏览器操作
